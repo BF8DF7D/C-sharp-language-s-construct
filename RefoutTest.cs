6a8958925b0820db521b1308aabc811188ebc0c5
Sus <nikitosik0202@yandex.ru>
2021-12-28 11:32:38 +0700

    Создание класса для демонстрации Ref и Out

---
+++ RefoutTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class RefOutTest
+{
+	private int value = 5;
+
+	public void UseRef(ref int number)
+    {
+		
+    }
+
+	public void UseOut(out int number)
+    {
+		number = value;
+    }
+	public RefOutTest()
+	{
+	}
+}
